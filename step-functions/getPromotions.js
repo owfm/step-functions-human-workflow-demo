a564ec1f1fc21c39fd537bef0c173cb5bcd714b6
Oliver Mansell <oliverwfmansell@gmail.com>
2020-05-12 18:58:51 +0100

    remove dynamodb lib

--- step-functions/getPromotions.js
+++ step-functions/getPromotions.js
@@ -1,5 +1,7 @@
 import handler from "./libs/handler-lib";
-import ddb from "./libs/dynamodb-lib";
+import AWS from "aws-sdk";
+
+const client = AWS.DynamoDb.DocumentClient();
 
 export const call = handler(async (event, context) => {
   const params = {
@@ -10,6 +12,6 @@ export const call = handler(async (event, context) => {
       ":decisionStatus": "decisionPending",
     },
   };
-  const result = await ddb.query(params);
+  const result = await client.query(params).promise();
   return result.Items;
 });
